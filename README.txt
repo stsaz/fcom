a463638768b1a6dcecd2059afc1b61d7b90ced1c
Simon Zolin <stsaz@yandex.ru>
2017-09-28 22:39:40 +0300

    + fcom un7z

--- README.txt
+++ README.txt
@@ -28,5 +28,6 @@ Features:
 			fcom unxz ./file1.xz -o ./file1.txt
 
 	fcom untar - unpack .tar files
+	fcom un7z - unpack .7z files
 		Unpack 2 files from gzip-compressed tar archive to the specified directory
 			fcom untar ./arc.tar.gz --member=file1.txt --member=file2.txt --outdir=mydir
--- src/arc.c
+++ src/arc.c
@@ -7,6 +7,7 @@ Copyright (c) 2017 Simon Zolin
 #include <FF/pack/gz.h>
 #include <FF/pack/xz.h>
 #include <FF/pack/tar.h>
+#include <FF/pack/7z.h>
 #include <FF/path.h>
 #include <FF/time.h>
 
@@ -56,6 +57,15 @@ static const fcom_filter untar_filt = { &untar_open, &untar_close, &untar_proces
 struct untar;
 static void untar_showinfo(struct untar *t, const fftar_file *f);
 
+// UN7Z
+static void* un7z_open(fcom_cmd *cmd);
+static void un7z_close(void *p, fcom_cmd *cmd);
+static int un7z_process(void *p, fcom_cmd *cmd);
+static const fcom_filter un7z_filt = { &un7z_open, &un7z_close, &un7z_process };
+
+struct un7z;
+static void un7z_showinfo(struct un7z *z, const ff7zfile *f);
+
 
 FF_EXP const fcom_mod* fcom_getmod(const fcom_core *_core)
 {
@@ -75,6 +85,7 @@ static const struct cmd cmds[] = {
 	{ "unxz", "arc.unxz", &unxz_filt },
 	{ "unxz1", NULL, &unxz1_filt },
 	{ "untar", "arc.untar", &untar_filt },
+	{ "un7z", "arc.un7z", &un7z_filt },
 };
 
 static int arc_sig(uint signo)
@@ -631,6 +642,167 @@ static void untar_showinfo(untar *t, const fftar_file *f)
 	p = ffs_copyz(p, end, f->name);
 
 	fcom_verblog(FILT_NAME, "%*s", p - t->fn.ptr, t->fn.ptr);
+}
+
+#undef FILT_NAME
+
+
+#define FILT_NAME  "arc.un7z"
+
+typedef struct un7z {
+	uint state;
+	ff7z z;
+	ffarr fn;
+	ffarr buf;
+} un7z;
+
+static void* un7z_open(fcom_cmd *cmd)
+{
+	un7z *z;
+	if (NULL == (z = ffmem_new(un7z)))
+		return FCOM_OPEN_SYSERR;
+
+	if (NULL == ffarr_alloc(&z->buf, BUFSIZE))
+		goto err;
+
+	if (NULL == ffarr_alloc(&z->fn, 4096))
+		goto err;
+
+	return z;
+
+err:
+	un7z_close(z, cmd);
+	return FCOM_OPEN_SYSERR;
+}
+
+static void un7z_close(void *p, fcom_cmd *cmd)
+{
+	un7z *z = p;
+	ffarr_free(&z->fn);
+	ffarr_free(&z->buf);
+	ff7z_close(&z->z);
+	ffmem_free(z);
+}
+
+static int un7z_process(void *p, fcom_cmd *cmd)
+{
+	un7z *z = p;
+	int r;
+	enum E { R_FIRST, R_NEXT, R_DATA, R_EOF, };
+
+	switch ((enum E)z->state) {
+	case R_EOF:
+		ff7z_close(&z->z);
+		ffmem_tzero(&z->z);
+		z->state = R_FIRST;
+		//fall through
+
+	case R_FIRST:
+		if (NULL == (cmd->input.fn = com->arg_next(cmd, 0)))
+			return FCOM_DONE;
+		com->ctrl(cmd, FCOM_CMD_FILTADD_PREV, FCOM_CMD_FILT_IN(cmd));
+		ff7z_open(&z->z);
+		z->state = R_DATA;
+		return FCOM_MORE;
+
+	case R_DATA:
+		break;
+
+	case R_NEXT:
+		FF_CMPSET(&cmd->output.fn, z->fn.ptr, NULL);
+		z->state = R_DATA;
+		break;
+	}
+
+	if (cmd->flags & FCOM_CMD_FWD) {
+		ff7z_input(&z->z, cmd->in.ptr, cmd->in.len);
+	}
+
+	for (;;) {
+
+	r = ff7z_read(&z->z);
+	switch (r) {
+
+	case FF7Z_FILEHDR:
+		for (;;) {
+			const ff7zfile *f;
+			if (NULL == (f = ff7z_nextfile(&z->z))) {
+				z->state = R_EOF;
+				return FCOM_MORE;
+			}
+
+			if (cmd->members.len != 0
+				&& 0 > ffs_findarrz((void*)cmd->members.ptr, cmd->members.len, f->name, ffsz_len(f->name)))
+				continue;
+
+			if (fcom_logchk(core->conf->loglev, FCOM_LOGVERB))
+				un7z_showinfo(z, f);
+			if (cmd->show)
+				continue;
+
+			if ((f->attr & FFWIN_FILE_DIR) && f->size != 0)
+				fcom_warnlog(FILT_NAME, "directory %s has non-zero size", f->name);
+
+			if (cmd->output.fn == NULL) {
+				ffstr name;
+				ffstr_setz(&name, f->name);
+				if (FCOM_DATA != (r = fn_out(cmd, &name, &z->fn)))
+					return r;
+				cmd->output.fn = z->fn.ptr;
+			}
+			cmd->output.size = f->size;
+			cmd->output.mtime = f->mtime;
+			cmd->output.attr = f->attr;
+			cmd->out_attr_win = 1;
+
+			const char *filt = (f->attr & FFWIN_FILE_DIR) ? "core.dir-out" : FCOM_CMD_FILT_OUT(cmd);
+			com->ctrl(cmd, FCOM_CMD_FILTADD, filt);
+			break;
+		}
+		break;
+
+	case FF7Z_DATA:
+		cmd->out = z->z.out;
+		return FCOM_DATA;
+
+	case FF7Z_FILEDONE:
+		z->state = R_NEXT;
+		return FCOM_NEXTDONE;
+
+	case FF7Z_MORE:
+		return FCOM_MORE;
+
+	case FF7Z_SEEK:
+		cmd->input.offset = ff7z_offset(&z->z);
+		cmd->in_seek = 1;
+		return FCOM_MORE;
+
+	case FF7Z_ERR:
+		fcom_errlog(FILT_NAME, "%s", ff7z_errstr(&z->z));
+		return FCOM_ERR;
+	}
+	}
+}
+
+/* "size date name" */
+static void un7z_showinfo(un7z *z, const ff7zfile *f)
+{
+	char *p = z->fn.ptr, *end = ffarr_edge(&z->fn);
+
+	if (f->attr & FFWIN_FILE_DIR)
+		p = ffs_copy(p, end, "       <DIR>", 12);
+	else
+		p += ffs_fromint(f->size, p, end - p, FFINT_WIDTH(12));
+	p = ffs_copyc(p, end, ' ');
+
+	ffdtm dt;
+	fftime_split(&dt, &f->mtime, FFTIME_TZLOCAL);
+	p += fftime_tostr(&dt, p, end - p, FFTIME_DATE_YMD | FFTIME_HMS);
+	p = ffs_copyc(p, end, ' ');
+
+	p = ffs_copyz(p, end, f->name);
+
+	fcom_verblog(FILT_NAME, "%*s", p - z->fn.ptr, z->fn.ptr);
 }
 
 #undef FILT_NAME
