32d7de5e8d4c57f6dace4fefff173d64bf45daa2
Simon Zolin <stsaz@yandex.ru>
2021-01-30 10:18:52 +0300

    * minor

--- test.sh
+++ test.sh
@@ -5,20 +5,22 @@ set -e # exit if a child process reports an error
 
 FCOM=./fcom
 TESTDIR=./fcom-test
-param=$1
+CMD=$1
 
-if test "$param" = "pack" ; then
+if test "$CMD" = "pack" ; then
 	rm -rf $TESTDIR
 	$FCOM gz ./README.txt -o $TESTDIR/test.gz
 	cat $TESTDIR/test.gz $TESTDIR/test.gz > $TESTDIR/test-double.gz
+	xz -c ./README.txt > $TESTDIR/README.txt.xz
 	mkdir -p emptydir
 	$FCOM tar emptydir ./README.txt ./CHANGES.txt -o $TESTDIR/test.tar
 	$FCOM tar emptydir ./README.txt ./CHANGES.txt -o $TESTDIR/test.tar.gz
 	$FCOM zip emptydir ./README.txt ./CHANGES.txt -o $TESTDIR/test.zip
 	$FCOM iso emptydir ./README.txt ./CHANGES.txt -o $TESTDIR/test.iso
+	7z a $TESTDIR/test.7z emptydir ./README.txt ./CHANGES.txt
 fi
 
-if test "$param" = "unpack" ; then
+if test "$CMD" = "unpack" ; then
 	rm -f $TESTDIR/*.txt
 	$FCOM ungz $TESTDIR/test.gz -C $TESTDIR
 	diff ./README.txt $TESTDIR/README.txt
@@ -28,9 +30,9 @@ if test "$param" = "unpack" ; then
 	cat ./README.txt ./README.txt > $TESTDIR/README.double.txt
 	diff $TESTDIR/README.double.txt $TESTDIR/README.txt
 
-	# rm -f $TESTDIR/*.txt
-	# $FCOM unxz ./fcom-xz.xz -C $TESTDIR
-	# diff ./README.txt $TESTDIR/README.txt
+	rm -f $TESTDIR/*.txt
+	$FCOM unxz $TESTDIR/README.txt.xz -C $TESTDIR
+	diff ./README.txt $TESTDIR/README.txt
 
 	rm -f $TESTDIR/*.txt
 	$FCOM untar $TESTDIR/test.tar -C $TESTDIR
@@ -53,9 +55,15 @@ if test "$param" = "unpack" ; then
 	diff ./CHANGES.txt $TESTDIR/CHANGES.txt
 
 	rm -f $TESTDIR/*.txt
+	$FCOM un7z $TESTDIR/test.7z -C $TESTDIR
+	diff ./README.txt $TESTDIR/README.txt
+	diff ./CHANGES.txt $TESTDIR/CHANGES.txt
+	ls $TESTDIR/emptydir
+
+	rm -f $TESTDIR/*.txt
 	$FCOM unpack $TESTDIR/test.gz -C $TESTDIR
-	# rm -f $TESTDIR/*.txt
-	# $FCOM unpack ./fcom-xz.xz -C $TESTDIR
+	rm -f $TESTDIR/*.txt
+	$FCOM unpack $TESTDIR/README.txt.xz -C $TESTDIR
 	rm -f $TESTDIR/*.txt
 	$FCOM unpack $TESTDIR/test.tar -C $TESTDIR
 	rm -f $TESTDIR/*.txt
@@ -66,13 +74,13 @@ if test "$param" = "unpack" ; then
 	$FCOM unpack $TESTDIR/test.iso -C $TESTDIR
 fi
 
-if test "$param" = "crypt" ; then
+if test "$CMD" = "crypt" ; then
 	$FCOM encrypt --password=123456 ./fcom -o $TESTDIR/fcom-encrypt -f
 	$FCOM decrypt --password=123456 $TESTDIR/fcom-encrypt -o $TESTDIR/fcom-decrypt -f
 	diff ./fcom $TESTDIR/fcom-decrypt
 fi
 
-if test "$param" = "all" ; then
+if test "$CMD" = "all" ; then
 	sh $0 pack
 	sh $0 unpack
 	sh $0 crypt
