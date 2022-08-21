3e82a265cbc454dba97fa50f066c09be411f550a
Simon Zolin <4729655+stsaz@users.noreply.github.com>
2022-08-21 11:42:38 +0300

    + copy

---
+++ v1/src/ops/copy.c
@@ -0,0 +1,295 @@
+/** fcom: copy data (input -> output)
+2022, Simon Zolin */
+
+#include <fcom.h>
+#include <FFOS/path.h>
+
+static const fcom_core *core;
+
+#define ffmem_free0(p)  ffmem_free(p), (p) = NULL
+
+#define BUF_LARGE  (8*1024*1024)
+
+struct copy {
+	uint st;
+	fcom_file_obj *in, *out;
+	ffstr data;
+	uint64 total, in_off, out_off;
+	uint nfiles;
+	char *iname, *oname;
+	fcom_cominfo *cmd;
+	byte preserve_date;
+	byte rename_source;
+	uint stop;
+};
+
+static int args_parse(struct copy *c, fcom_cominfo *cmd)
+{
+	static const ffcmdarg_arg args[] = {
+		{ 0,	"rename-source",	FFCMDARG_TSWITCH, FF_OFF(struct copy, rename_source) },
+		{}
+	};
+	return core->com->args_parse(cmd, args, c);
+}
+
+static const char* copy_help()
+{
+	return "\
+Copy files from one place to another.\n\
+Implies '--recursive'.\n\
+Uses large '--buffer' by default.\n\
+Usage:\n\
+  fcom copy INPUT... [-o OUTPUT_FILE] [-C OUTPUT_DIR] [OPTIONS]\n\
+    OPTIONS:\n\
+        --rename-source\n\
+                        Rename source file to *.deleted after successful operation\n\
+";
+}
+
+static void copy_close(fcom_op *op);
+
+static fcom_op* copy_create(fcom_cominfo *cmd)
+{
+	struct copy *c = ffmem_new(struct copy);
+	c->preserve_date = 1;
+	if (cmd->buffer_size == 0)
+		cmd->buffer_size = BUF_LARGE;
+
+	if (0 != args_parse(c, cmd))
+		goto end;
+	cmd->recursive = 1;
+
+	struct fcom_file_conf fc = {};
+	fc.buffer_size = cmd->buffer_size;
+	fc.n_buffers = 1;
+	c->in = core->file->create(&fc);
+	c->out = core->file->create(&fc);
+
+	c->cmd = cmd;
+	return c;
+
+end:
+	copy_close(c);
+	return NULL;
+}
+
+static void copy_reset(struct copy *c)
+{
+	c->total = 0;
+	c->in_off = c->out_off = 0;
+
+	ffmem_free0(c->iname);
+	ffmem_free0(c->oname);
+}
+
+static void copy_close(fcom_op *op)
+{
+	struct copy *c = op;
+	core->file->destroy(c->in);
+	core->file->destroy(c->out);
+
+	ffmem_free(c->iname);
+	ffmem_free(c->oname);
+	ffmem_free(c);
+}
+
+static void copy_signal(fcom_op *op, uint signal)
+{
+	struct copy *c = op;
+	FFINT_WRITEONCE(c->stop, 1);
+}
+
+static char* out_name(struct copy *c, ffstr in, ffstr base)
+{
+	char *s;
+	if (c->cmd->output.len != 0 && c->cmd->chdir.len == 0) {
+		s = ffsz_dupstr(&c->cmd->output);
+
+	} else if (c->cmd->output.len != 0 && c->cmd->chdir.len != 0) {
+		s = ffsz_allocfmt("%S%c%S"
+			, &c->cmd->chdir, FFPATH_SLASH, &c->cmd->output);
+
+	} else if (c->cmd->output.len == 0 && c->cmd->chdir.len != 0) {
+		/*
+		`in -C out`: "in" -> "out/in"
+		`d -R -C out`: "d/f" -> "out/d/f"
+		`/tmp/d -R -C out`: "/tmp/d/f" -> "out/d/f"
+		*/
+		ffstr name;
+		if (base.len == 0)
+			base = in;
+		ffpath_splitpath(base.ptr, base.len, NULL, &name);
+		if (name.len != 0)
+			ffstr_shift(&in, name.ptr - base.ptr);
+		s = ffsz_allocfmt("%S%c%S"
+			, &c->cmd->chdir, FFPATH_SLASH, &in);
+
+	} else {
+		fcom_errlog("please use --output or --chdir to set destination");
+		return NULL;
+	}
+
+	fcom_dbglog("output file name: %s", s);
+	return s;
+}
+
+static void copy_run(fcom_op *op)
+{
+	struct copy *c = op;
+	int r, k = 0;
+	enum {
+		I_SRC, I_OPEN_IN, I_MKDIR, I_OPEN_OUT,
+		I_READ, I_WRITE, I_RD_DONE, I_DONE,
+	};
+	while (!FFINT_READONCE(c->stop)) {
+		switch (c->st) {
+
+		case I_SRC: {
+			ffstr name, base;
+			if (0 != core->com->input_next(c->cmd, &name, &base, 0)) {
+				if (c->nfiles == 0) {
+					fcom_errlog("no input files");
+					goto end;
+				}
+				k = 1;
+				goto end;
+			}
+			c->nfiles++;
+			c->iname = ffsz_dupstr(&name);
+
+			if (!c->cmd->stdout) {
+				if (NULL == (c->oname = out_name(c, name, base)))
+					goto end;
+			}
+
+			c->st++;
+		}
+			// fallthrough
+
+		case I_OPEN_IN: {
+			uint flags = FCOM_FILE_READ;
+			flags |= fcom_file_cominfo_flags_i(c->cmd);
+			r = core->file->open(c->in, c->iname, flags);
+			if (r == FCOM_FILE_ERR) goto end;
+
+			fffileinfo fi = {};
+			r = core->file->info(c->in, &fi);
+			if (r == FCOM_FILE_ERR) goto end;
+			if (fffile_isdir(fffileinfo_attr(&fi))) {
+				c->st = I_MKDIR;
+				continue;
+			}
+
+			c->st = I_OPEN_OUT;
+			continue;
+		}
+
+		case I_MKDIR: {
+			r = core->file->dir_create(c->oname, 0);
+			if (r == FCOM_FILE_ERR) goto end;
+			// move our directory descriptor to Com, so it can use it for fdopendir()
+			fffd fd = core->file->fd(c->in, FCOM_FILE_ACQUIRE);
+			core->com->input_dir(c->cmd, fd);
+			c->st = I_SRC;
+			continue;
+		}
+
+		case I_OPEN_OUT: {
+			uint flags = FCOM_FILE_WRITE;
+			flags |= fcom_file_cominfo_flags_o(c->cmd);
+
+			r = core->file->open(c->out, c->oname, flags);
+			if (r == FCOM_FILE_ERR) goto end;
+
+			if (!c->cmd->stdout) {
+				fffileinfo fi = {};
+				r = core->file->info(c->out, &fi);
+				if (r == FCOM_FILE_ERR) goto end;
+				if (fffile_isdir(fffileinfo_attr(&fi))) {
+					fcom_errlog("output file is an existing directory. Use '-C DIR' to copy files into this directory.");
+					goto end;
+				}
+			}
+
+			core->file->behaviour(c->in, FCOM_FBEH_SEQ);
+			c->st++;
+		}
+			// fallthrough
+
+		case I_READ:
+			r = core->file->read(c->in, &c->data, c->in_off);
+			if (r == FCOM_FILE_ERR) goto end;
+			if (r == FCOM_FILE_EOF) {
+				c->st = I_RD_DONE;
+				continue;
+			}
+			c->in_off += c->data.len;
+
+			c->st = I_WRITE;
+			continue;
+
+		case I_WRITE:
+			r = core->file->write(c->out, c->data, c->out_off);
+			if (r == FCOM_FILE_ERR) goto end;
+			c->out_off += c->data.len;
+			c->total += c->data.len;
+
+			c->st = I_READ;
+			continue;
+
+		case I_RD_DONE:
+			core->file->behaviour(c->out, FCOM_FBEH_TRUNC_PREALLOC);
+			if (c->preserve_date) {
+				fftime mtime;
+				if (0 == core->file->mtime(c->in, &mtime))
+					core->file->mtime_set(c->out, &mtime);
+			}
+
+			c->st = I_DONE;
+			continue;
+
+		case I_DONE:
+			if (c->rename_source) {
+				char *fn = ffsz_allocfmt("%s.deleted", c->iname);
+				if (0 != fffile_rename(c->iname, fn)) {
+					fcom_syserrlog("file rename: '%s' -> '%s'", c->iname, fn);
+				}
+				ffmem_free(fn);
+			}
+
+			fcom_verblog("'%s' -> '%s', %,U"
+				, c->iname, c->oname, c->total);
+
+			copy_reset(c);
+			c->st = I_SRC;
+			continue;
+		}
+	}
+
+end:
+	{
+	fcom_cominfo *cmd = c->cmd;
+	copy_close(c);
+	core->com->destroy(cmd);
+	}
+	if (!k)
+		core->exit(1);
+	else
+		core->exit(0);
+}
+
+FF_EXP const fcom_operation copy = {
+	copy_create,
+	copy_close,
+	copy_run,
+	copy_signal,
+	copy_help,
+};
+
+
+static void copy_init(const fcom_core *_core) { core = _core; }
+static void copy_destroy() {}
+FF_EXP const struct fcom_module fcom_module = {
+	FCOM_VER,
+	copy_init, copy_destroy,
+};
